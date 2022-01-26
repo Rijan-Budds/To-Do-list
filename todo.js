516ae4d5eec2646663cee31d3125dcc4dcce4942
RijanBudds <rijanbudds@gmail.com>
2022-01-26 13:52:51 +0545

    first commit

--- todo.js
+++ todo.js
@@ -1,7 +1,17 @@
-function todoList(){
-    let item = document.getElementById("todoInput").value
-    let text = document.createTextNode(item)
-    let newItem = document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("todoList").appendChild(newItem)
-}  
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through"; 
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
